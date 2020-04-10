2456d463e47cf402368293f0549534b23d35ce02
Liam <liaminscnu@outlook.com>
2020-04-10 18:36:25 +0800

    progress bar added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,38 @@
 
+// Progress bar according to images
+function progressBar() {
+
+    // Reference: 
+    // https://www.jianshu.com/p/7d702813b684
+    // https://www.jianshu.com/p/bba0103cddf7/
+    
+    var imgs = $("img");
+    var img_num = 0;
+    imgs.each(function (i) {
+        var oImg=new Image();
+        // console.log(oImg);
+        oImg.onload = function(){//使用onload方法，在加载完成后执行
+            oImg.onload = null;//首先清除掉缓存
+            img_num++;//每次加载的过程中num++，即执行次数
+            var con_b = parseInt(img_num / ($("img").length) * 100)+"%";
+            console.log(con_b);
+            $('.progress-bar').css({'width': con_b});
+        }
+        // ?
+        oImg.src = imgs[i].src;//预加载，先指定一个img.src，当onload成功以后可以将数据指定到某一个元素或者图片上，或者返回一个结果
+    });
+
+    setInterval(function() {
+        $(".progress-bar").fadeOut();
+    }, 1000)
+
+
+}
+
 $(document).ready(function() {
 
-    // Progress Bar
-    // Reference: https://www.jianshu.com/p/7d702813b684
-    // 页面加载完成后1秒进度条消失
-    setInterval(function() {
-        $('.progress-bar').fadeOut()
-    }, 3000);
-
+    // Run progress bar
+    progressBar();
 
     // Project card link to page
     $("#onecooperation").on("click", function() {
@@ -34,4 +59,4 @@ $(document).ready(function() {
         window.location.href = "work/ritparking_zh";
     });
 
-})
+});
